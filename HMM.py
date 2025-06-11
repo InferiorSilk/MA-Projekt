a7ec6df5a0999903df31fb715eb04e855da632dd
Aaron Wulf <wulf.aaron@outlook.com>
2025-06-11 12:15:44 +0200

    refactor: Refactor the entire HMM into an object, improve logic.

--- HMM.py
+++ HMM.py
@@ -5,131 +5,194 @@
 """
 from conllu import parse_incr
 import json
+import copy
+import os
 
-states = upos_tags = [
-    "ADJ",   # Adjective
-    "ADP",   # Adposition
-    "ADV",   # Adverb
-    "AUX",   # Auxiliary
-    "CCONJ", # Coordinating conjunction
-    "DET",   # Determiner
-    "INTJ",  # Interjection
-    "NOUN",  # Noun
-    "NUM",   # Numeral
-    "PART",  # Particle
-    "PRON",  # Pronoun
-    "PROPN", # Proper noun
-    "PUNCT", # Punctuation
-    "SCONJ", # Subordinating conjunction
-    "SYM",   # Symbol
-    "VERB",  # Verb
-    "X"      # Other
-]
+class HMM:
+    UPOS_TAGS = [
+        "ADJ",   # Adjective
+        "ADP",   # Adposition
+        "ADV",   # Adverb
+        "AUX",   # Auxiliary
+        "CCONJ", # Coordinating conjunction
+        "DET",   # Determiner
+        "INTJ",  # Interjection
+        "NOUN",  # Noun
+        "NUM",   # Numeral
+        "PART",  # Particle
+        "PRON",  # Pronoun
+        "PROPN", # Proper noun
+        "PUNCT", # Punctuation
+        "SCONJ", # Subordinating conjunction
+        "SYM",   # Symbol
+        "VERB",  # Verb
+        "X"      # Other
+    ]
 
-observations = set()
-
-start_prob = {state: 0 for state in states}
-
-trans_prob = {state: {state_to: 0 for state_to in states} for state in states}
-
-emit_prob = {state: {} for state in states}
-
-total_words = 0
-total_first_words = 0
-total_transitions = 0
-total_tag_count = {state: 0 for state in states}
-
-global hmm_params
-hmm_params = {
-    'states': states,
-    'observations': observations,
-    'start_prob': start_prob,
-    'trans_prob': trans_prob,
-    'emit_prob': emit_prob,
-    'total_words': total_words,
-    'total_first_words': total_first_words,
-    'total_transitions': total_transitions,
-    'total_tag_count': total_tag_count
-}
-
-def write_params():
-    with open("hmm_params.json", "w", encoding="utf-8") as f:
-        json.dump(hmm_params, f, indent=2)
-
-def read_params():
-    with open('hmm_params.json', 'r', encoding='utf-8') as f:
-        hmm_params = json.load(f)
-        return hmm_params
-
-def read_observations():
-    with open("UD_English-GUM/en_gum-ud-train.conllu", "r", encoding="utf-8") as f:
-        for tokenlist in parse_incr(f):
-            words = [token["form"] for token in tokenlist if isinstance(token["id"], int)]
-            observations.update(words)
-            train_emit(words)
-
-def train(sentence):
-    if not sentence:
-        return
-    # Train first words
-    train_first(sentence)
-    # Train transitions
-    train_trans(sentence)
-
-    for word, tag in sentence:
-        # Add word to observations
-        if word not in hmm_params["observations"]:
-            hmm_params["observations"].add(word)
-    
-        # Emitions and stuff
-        if word not in hmm_params['emit_prob'][tag]:
-            hmm_params['emit_prob'][tag][word] = 0
-        hmm_params['emit_prob'][tag][word] += 1
-        # Tag counts
-        hmm_params['total_tag_count'][tag] += 1
-        # Total words
-        hmm_params["total_words"] += 1
-
-def train_emit(words):
-    for word in words:
-        if word in emit_prob:
-            emit_prob[word]["tag"] += 1
-        else:
-            emit_prob[word] = 1
+    def __init__(self):
+        self.states = self.UPOS_TAGS
+        self.observations = set()
         
-def train_first(sentence):
-    first_tag = sentence[0][1]
-    hmm_params["start_prob"][first_tag] += 1
-    hmm_params["total_first_words"] += 1
+        self.start_counts = {state: 0 for state in self.states}
+        self.transition_counts = {state: {state_to: 0 for state_to in self.states} for state in self.states}
+        self.emission_counts = {state: {} for state in self.states}
+        
+        self.total_words = 0
+        self.total_first_words = 0
+        self.total_transitions = 0
+        self.tag_counts = {state: 0 for state in self.states}
 
-def train_trans(sentence):
-    for i in range(len(sentence) - 1):
-        hmm_params["total_transitions"] += 1
-        current_tag = sentence[i][1]
-        next_tag = sentence[i+1][1]
-        hmm_params["trans_prob"][current_tag][next_tag] += 1
+    def write_params(self, filepath="hmm_params.json"):
+        params = {
+            'states': self.states,
+            'observations': list(self.observations), # Convert set to list for JSON
+            'start_prob': self.start_counts,          # Using 'start_prob' key as in original JSON
+            'trans_prob': self.transition_counts,     # Using 'trans_prob' key
+            'emit_prob': self.emission_counts,        # Using 'emit_prob' key
+            'total_words': self.total_words,
+            'total_first_words': self.total_first_words,
+            'total_transitions': self.total_transitions,
+            'total_tag_count': self.tag_counts
+        }
+        with open(filepath, "w", encoding="utf-8") as f:
+            json.dump(params, f, indent=2)
+        print(f"HMM parameters saved to {filepath}")
 
-def calculate_prob():
-    # Calculate Transition Probabilities 
-    for current_tag in hmm_params['trans_prob']:
-        total_transitions_from_current = sum(hmm_params['trans_prob'][current_tag].values())
-        if total_transitions_from_current > 0:
-            for next_tag in hmm_params['trans_prob'][current_tag]:
-                hmm_params['trans_prob'][current_tag][next_tag] /= total_transitions_from_current
+    def read_params(self, filepath="hmm_params.json"):
+        try:
+            with open(filepath, 'r', encoding='utf-8') as f:
+                loaded_params = json.load(f)
+                
+                self.observations = set(loaded_params.get('observations', []))
+                self.start_counts = loaded_params.get('start_prob', self.start_counts)
+                self.transition_counts = loaded_params.get('trans_prob', self.transition_counts)
+                self.emission_counts = loaded_params.get('emit_prob', self.emission_counts)
+                self.total_words = loaded_params.get('total_words', 0)
+                self.total_first_words = loaded_params.get('total_first_words', 0)
+                self.total_transitions = loaded_params.get('total_transitions', 0)
+                self.tag_counts = loaded_params.get('total_tag_count', self.tag_counts)
+                print(f"HMM parameters loaded from {filepath}")
+
+        except FileNotFoundError:
+            print(f"Warning: {filepath} not found. Initializing with empty parameters.")
+        except json.JSONDecodeError:
+            print(f"Error: Could not decode {filepath}. File might be corrupted. Initializing with empty parameters.")
+            self.__init__() # Reset to default (for safety)
+
+    def _train_first(self, sentence_tags):
+        if not sentence_tags:
+            return
+        first_tag = sentence_tags[0]
+        self.start_counts[first_tag] += 1
+        self.total_first_words += 1
+
+    def _train_trans(self, sentence_tags):
+        if len(sentence_tags) < 2:
+            return
+        for i in range(len(sentence_tags) - 1):
+            current_tag = sentence_tags[i]
+            next_tag = sentence_tags[i+1]
+            self.transition_counts[current_tag][next_tag] += 1
+            self.total_transitions += 1 # Increment for each valid transition pair
+
+    def train(self, sentence_word_tag_pairs):
+        if not sentence_word_tag_pairs:
+            return
+
+        sentence_tags = [tag for word, tag in sentence_word_tag_pairs]
+
+        # Train first words based on tags
+        self._train_first(sentence_tags)
+        # Train transitions based on tags
+        self._train_trans(sentence_tags)
+
+        for word, tag in sentence_word_tag_pairs:
+            # Add word to observations
+            self.observations.add(word)
     
-    # Calculate Start Probabilities 
-    if hmm_params["total_first_words"] > 0:
-        for tag in hmm_params["start_prob"]:
-            hmm_params["start_prob"][tag] /= hmm_params["total_first_words"]
+            # Emission counts
+            if word not in self.emission_counts[tag]:
+                self.emission_counts[tag][word] = 0
+            self.emission_counts[tag][word] += 1
+            
+            # Tag counts
+            self.tag_counts[tag] += 1
+            # Total words
+            self.total_words += 1
+        
+    def get_probabilities(self):
+        # Create a structure for probabilities, copying counts and other relevant info
+        prob_params = {
+            'states': list(self.states), # Copy of states
+            'observations': set(self.observations), # Copy of observations
+            # Deepcopies recommended by AI
+            'start_prob': copy.deepcopy(self.start_counts),
+            'trans_prob': copy.deepcopy(self.transition_counts),
+            'emit_prob': copy.deepcopy(self.emission_counts),
+            'total_words': self.total_words,
+            'total_first_words': self.total_first_words,
+            'total_transitions': self.total_transitions,
+            'total_tag_count': copy.deepcopy(self.tag_counts)
+        }
 
-    # Calculate Emission Probabilities 
-    for tag in hmm_params['emit_prob']:
-        total_occurrences_of_tag = hmm_params['total_tag_count'].get(tag, 0)
-        if total_occurrences_of_tag > 0:
-            for word in hmm_params['emit_prob'][tag]:
-                hmm_params['emit_prob'][tag][word] /= total_occurrences_of_tag
+        # Calculate Transition Probabilities
+        for current_tag in prob_params['trans_prob']:
+            total_transitions_from_current = sum(prob_params['trans_prob'][current_tag].values())
+            if total_transitions_from_current > 0:
+                for next_tag in prob_params['trans_prob'][current_tag]:
+                    prob_params['trans_prob'][current_tag][next_tag] /= total_transitions_from_current
+            else: # TODO: Laplace smoothing
+                for next_tag in prob_params['trans_prob'][current_tag]:
+                    prob_params['trans_prob'][current_tag][next_tag] = 0.0
 
-with open("UD_English-GUM/en_gum-ud-train.conllu", "r", encoding="utf-8") as f:
-    for tokenlist in parse_incr(f):
-        sentence = [(token["form"], token["upostag"]) for token in tokenlist if isinstance(token["id"], int)]
-        train(sentence)
+
+        # Calculate Start Probabilities
+        if prob_params["total_first_words"] > 0:
+            for tag in prob_params["start_prob"]:
+                prob_params["start_prob"][tag] /= prob_params["total_first_words"]
+        else:
+             for tag in prob_params["start_prob"]:
+                prob_params["start_prob"][tag] = 0.0
+
+
+        # Calculate Emission Probabilities
+        for tag in prob_params['emit_prob']:
+            total_occurrences_of_tag = prob_params['total_tag_count'].get(tag, 0)
+            if total_occurrences_of_tag > 0:
+                for word in prob_params['emit_prob'][tag]:
+                    prob_params['emit_prob'][tag][word] /= total_occurrences_of_tag
+            else: # First time of the tag
+                for word in prob_params['emit_prob'][tag]: # Hopefully
+                    prob_params['emit_prob'][tag][word] = 0.0
+            
+        return prob_params
+
+if __name__ == "__main__":
+    model = HMM()
+    
+    # Try to load params
+    model.read_params("hmm_params.json") 
+
+    # Define the training data file path
+    training_file = "UD_English-GUM/en_gum-ud-train.conllu"
+
+    if not os.path.exists(training_file):
+        print(f"Error: Training file not found at {training_file}")
+        print("Please ensure the CoNLL-U file is in the correct location.")
+    else:
+        print(f"Starting training with data from {training_file}...")
+        sentence_count = 0
+        with open(training_file, "r", encoding="utf-8") as f:
+            for tokenlist in parse_incr(f):
+                sentence = []
+                for token in tokenlist:
+                    if isinstance(token["id"], int) and token["form"] is not None and token["upostag"] is not None:
+                        sentence.append((token["form"].lower(), token["upostag"])) # Normalize words to lowercase
+                
+                if sentence: # Make sure theres something
+                    model.train(sentence)
+                    sentence_count += 1
+        
+        print(f"Training completed. Processed {sentence_count} sentences.")
+        # Save params
+        model.write_params("hmm_params.json")
